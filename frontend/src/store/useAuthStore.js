a7886fb5d5edb4ccd0858b073c65b8ad5cb2f629
THEJUSKRISHNAN <thejus12a2003@gmail.com>
2025-04-26 01:48:51 +0530

    login,logout,signup added in frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import {create} from "zustand"
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
     authUser: null,
@@ -19,5 +20,45 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({isCheckingAuth:false})
         }
-    }
+    },
+
+    signup: async( data ) => {
+        set({ isSingningUp: true})
+        try {
+            const res = await axiosInstance.post("/auth/signup",data);
+            set({authUser:res.data});
+            toast.success("Accound created successfully");
+            
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isSingningUp:false})
+        }
+
+    },
+
+    login: async(data)=> {
+        set({ isLoggingIng: true});
+
+        try {
+            const res = await axiosInstance.post("/auth/login",data);
+            set({authUser: res.data});
+            toast.success("Logged in successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({isLoggingIng: false});
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({authUser: null})
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    },
+
+
 }));
